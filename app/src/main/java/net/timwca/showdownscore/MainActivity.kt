d5b09c1ba7767c5626d8493e258e090c614b4ec9
Timofei Vikhrianov <vihrianov1@outlook.com>
2022-01-08 01:21:16 +0700

    Готов интерфейс

--- app/src/main/java/net/timwca/showdownscore/MainActivity.kt
+++ app/src/main/java/net/timwca/showdownscore/MainActivity.kt
@@ -1,11 +1,24 @@
 package net.timwca.showdownscore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+    }
+
+    var servNum : Int = 1 // Номер подачи (1-я или 2-я)
+
+    fun btnServingA(view: View){ // Первая подача A
+        val serv : TextView = findViewById(R.id.serving)
+        serv.text = "Подача: Игрок A - $servNum"
+        servNum++
     }
 }
