0183afbf268100c8822a9aef3b88f739e8d18d88
Timofei Vikhrianov <vihrianov1@outlook.com>
2022-08-29 22:20:41 +0700

    Добавлено Activity для командного режима (TeamModeActivity)

---
+++ app/src/main/java/net/timwca/showdownscore/TeamModeActivity.kt
@@ -0,0 +1,11 @@
+package net.timwca.showdownscore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TeamModeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_team_mode)
+    }
+}
